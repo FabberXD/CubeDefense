c12b0dc1be4fbf3c37a668831f1a710e764aef5d
SysFAB64 <simplehuman228@gmail.com>
2024-01-27 19:13:57 +0200

    Update map.lua

--- lua/map.lua
+++ lua/map.lua
@@ -4,10 +4,17 @@ local map = function(config)
 	local defaultConfig = {
 		name = "",
 		shape = "",
+		playerScale = 1,
 	}
 
 	for key, value in pairs(defaultConfig) do
 		t[key] = config[key] or defaultConfig[key]
+	end
+
+	t.load = function(self)
+		local shape = MutableShape(shape)
+		World:AddChild(shape)
+		return shape
 	end
 
 	return t
