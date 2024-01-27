057fd4c0e01aaa63c14ae9c2cb55768eac7b9ad6
SysFAB64 <simplehuman228@gmail.com>
2024-01-27 19:27:38 +0200

    Update map.lua

--- lua/map.lua
+++ lua/map.lua
@@ -7,7 +7,11 @@ local map = function(config)
 	local defaultConfig = {
 		name = "",
 		shape = "",
+		scale = 1,
 		playerScale = 1,
+
+		spawnPos = Number3(0, 0, 0),
+		endPos = Number3(0, 0, 0),
 	}
 
 	for key, value in pairs(defaultConfig) do
@@ -15,9 +19,12 @@ local map = function(config)
 	end
 
 	t.load = function(self)
-		local shape = MutableShape(shape)
-		World:AddChild(shape)
-		return shape
+		local mapShape = MutableShape(self.shape)
+		World:AddChild(mapShape)
+
+		mapShape.Scale = self.scale
+
+		return mapShape
 	end
 
 	return t
