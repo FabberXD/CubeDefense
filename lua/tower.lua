8eac7b634011269fb79b45070e12d553eef8ac36
Nanskipp <nanskip.oof@gmail.com>
2024-01-30 00:01:31 +0200

    Update tower.lua
    added burst time to towers

--- lua/tower.lua
+++ lua/tower.lua
@@ -9,6 +9,7 @@ local tower = function(config)
 		name = "",
 		base_damage = 1,
 		base_burst = 1,
+		base_burst_time = 0.1,
 		base_reload_time = 1,
 		base_radius = 1,
 		is_top = false,
@@ -25,6 +26,7 @@ local tower = function(config)
 	t.recalculateStats = function(self)
 		self.damage = self.base_damage * (self.damage_multiplier or 1)
 		self.burst = self.base_burst
+		self.burst_time = self.base_burst_time * (self.burst_time_multiplier or 1)
 		self.reload_time = self.base_reload_time * (self.reload_multiplier or 1)
 		self.radius = self.base_radius * (self.radius_multiplier or 1)
 	end
