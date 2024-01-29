41de81f788f214982cbacb8318961246bfdc4727
SysFAB64 <simplehuman228@gmail.com>
2024-01-30 00:53:28 +0200

    Client&Server update
    impulse initialization

--- lua/client.lua
+++ lua/client.lua
@@ -1,5 +1,9 @@
 local client = function(impulse, game, map, tower, enemy)
 	local t = {}
+
+	t.impulse = impulse()
+	t.onPlayerJoin = t.impulse.startConnection
+
 	return t
 end
 return client
--- lua/server.lua
+++ lua/server.lua
@@ -1,5 +1,8 @@
 local server = function(impulse, game, map, tower, enemy, towers, maps)
 	local t = {}
+
+	t.impulse = impulse()
+
 	return t
 end
 return server
