ad2a259311f97fb889ea94ef09da31859ab50ae1
SysFAB64 <simplehuman228@gmail.com>
2024-01-30 00:48:04 +0200

    Added client and server main scripts

---
+++ lua/client.lua
@@ -0,0 +1,5 @@
+local client = function(impulse, game, map, tower, enemy)
+	local t = {}
+	return t
+end
+return client
---
+++ lua/server.lua
@@ -0,0 +1,5 @@
+local server = function(impulse, game, map, tower, enemy, towers, maps)
+	local t = {}
+	return t
+end
+return server
