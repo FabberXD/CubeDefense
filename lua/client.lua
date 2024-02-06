6920f442314dbd72709d795a693074e07da9dcdf
SysFAB64 <simplehuman228@gmail.com>
2024-02-06 23:53:23 +0200

    Update client.lua

--- lua/client.lua
+++ lua/client.lua
@@ -4,6 +4,10 @@ local client = function()
 	t.impulse = impulse()
 	t.OnPlayerJoin = t.impulse.startConnection
 
+	Camera:SetModeFree()
+	World:AddChild(Player)
+	Player.IsHidden = true
+
 	return t
 end
 return client
